0390fe5ccd8b9cd9a54ab57bc3c24ca4cd33cf99
Paul Klemstine <paulklemstine@gmail.com>
2026-07-17 22:30:19 -0500

    Update project screenshots with live captures and hide Cosmos project

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm install 20
+nvm use 20
+cd /home/raver1975/portfolio
+npx firebase-tools deploy
